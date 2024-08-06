e07a2bd66e196b3e6b4351fae3a8c5a1c2d851ee
Robert Jarrett <howtotech77@gmail.com>
2024-08-06 13:37:06 -0500

    Update recent.js

--- pages/en/anime/recent.js
+++ pages/en/anime/recent.js
@@ -75,7 +75,7 @@ export default function Recent({ sessions }) {
         <meta name="title" content="New Episodes" />
         <meta
           name="description"
-          content="Explore Beloved Classics and Favorites - Dive into a curated collection of timeless anime on Makima's New Episodes Page. From iconic classics to all-time favorites, experience the stories that have captured hearts worldwide. Start streaming now and relive the magic of anime!"
+          content="Explore Beloved Classics and Favorites - Dive into a curated collection of timeless anime on Makima's New Episodes Page. From iconic classics to all-time favorites, experience the stories that have captured hearts worldwide. Start streaming now and relive the magic of anime on makima!"
         />
       </Head>
       <MobileNav sessions={sessions} />
